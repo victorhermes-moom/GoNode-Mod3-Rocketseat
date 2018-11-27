cccd3323a46b3ee4982e7716bf69a3c2f66d8bf2
Victor Hermes <me.victorhermes@gmail.com>
2018-11-27 19:37:23 -0200

    Mongoose

---
+++ src/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  uri:
+    'mongodb://gonode:gonode123@ds217864.mlab.com:17864/gonode-mod3-rocketseat'
+}
--- src/server.js
+++ src/server.js
@@ -1,19 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
+
 class App {
-  constructor() {
-    this.express = express();
-    this.isDev = process.env.NODE_ENV !== "production"; // var responsável por saber se o ambiente está em desenvolvimento ou não
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV !== 'production' // var responsável por saber se o ambiente está em desenvolvimento ou não
 
-    this.middlewares();
-    this.routes();
+    this.database()
+    this.middlewares()
+    this.routes()
   }
 
-  middlewares() {
-    this.express.use(express.json());
+  database () {
+    mongoose.connect(
+      databaseConfig.uri,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true
+      }
+    )
   }
 
-  routes() {
-    this.express.use(require("./routes"));
+  middlewares () {
+    this.express.use(express.json())
+  }
+
+  routes () {
+    this.express.use(require('./routes'))
   }
 }
 
-module.exports = new App().express;
+module.exports = new App().express
