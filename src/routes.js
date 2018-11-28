15a1bbb4dcd476702d09240d749d81c9559754fc
Victor Hermes <me.victorhermes@gmail.com>
2018-11-28 21:13:14 -0200

    Token JWT

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,13 @@ const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
+const authMiddlewares = require('./app/middlewares/auth')
+
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+
+routes.get('/teste', authMiddlewares, (req, res) =>
+  res.json({ ok: 'autenticado' })
+)
 
 module.exports = routes
