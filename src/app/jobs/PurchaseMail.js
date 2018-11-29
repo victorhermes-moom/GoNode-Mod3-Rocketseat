0f6b07cd62613f53a2be5b48c1fef1301f7eee3b
Victor Hermes <me.victorhermes@gmail.com>
2018-11-29 21:33:51 -0200

    Redis

---
+++ src/app/jobs/PurchaseMail.js
@@ -0,0 +1,23 @@
+const Mail = require('../services/Mail')
+
+class PurchaseMail {
+  get key () {
+    return 'PurchaseMail'
+  }
+
+  async handle (job, done) {
+    const { ad, user, content } = job.data
+
+    await Mail.sendMail({
+      from: '"Victor Hermes" <victor@moom.com.br>',
+      to: ad.author.email,
+      subject: `Solicitação de compra: ${ad.title}`,
+      template: 'purchase',
+      context: { user, content, ad }
+    })
+
+    return done()
+  }
+}
+
+module.exports = new PurchaseMail()
