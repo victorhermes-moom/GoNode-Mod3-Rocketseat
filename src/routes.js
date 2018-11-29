3d49b76cc755359b64040229719e7c5600fa86e4
Victor Hermes <me.victorhermes@gmail.com>
2018-11-29 21:44:11 -0200

    Validação com express-validation e Joi

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,38 @@
 const express = require('express')
+const validate = require('express-validation')
+
 const routes = express.Router()
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
 const authMiddlewares = require('./app/middlewares/auth')
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 routes.use(authMiddlewares)
 
 // Ads
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 // Purchase
-routes.post('/purchase', controllers.PurchaseController.store)
+routes.post(
+  '/purchase',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
