09bb435570f2a8cddb5ca77eb8e42db6dae6cc43
Victor Hermes <me.victorhermes@gmail.com>
2018-11-29 18:34:56 -0200

    Template email com hbs

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -13,7 +13,8 @@ class PurchaseController {
       from: '"Victor Hermes" <victor@moom.com.br>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: '<p>Apenas um teste!</p>'
+      template: 'purchase',
+      context: { user, content, ad: purchaseAd }
     })
 
     return res.send()
